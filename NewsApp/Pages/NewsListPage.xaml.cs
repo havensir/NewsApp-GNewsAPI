cb19d73283dfb12b20ea56cc1bd58192dfec4f0f
ocahavens <ocahavens@gmail.com>
2026-03-06 12:40:59 -0500

    Redesign NewsListPage with article list and navigation
    Refactored NewsListPage UI to use a Grid layout with a CollectionView for displaying articles. Added data binding, async news fetching by category, and navigation to NewsDetailPage on article selection.

--- NewsApp/Pages/NewsListPage.xaml.cs
+++ NewsApp/Pages/NewsListPage.xaml.cs
@@ -1,9 +1,39 @@
+using NewsApp.Services;
+using NewsApp.Models;
+
 namespace NewsApp.Pages;
 
 public partial class NewsListPage : ContentPage
 {
-	public NewsListPage()
-	{
-		InitializeComponent();
-	}
+    public NewsListPage(string categoryName)
+    {
+        Title = categoryName;
+        InitializeComponent();
+        BindingContext = this;
+        ArticleList = new List<Article>();
+        GetNews(categoryName);
+    }
+
+    public List<Article> ArticleList;
+
+    private async Task GetNews(string categoryName)
+    {
+        var apiService = new ApiService();
+        var newsResult = await apiService.GetNews(categoryName);
+        foreach (var item in newsResult.Articles)
+        {
+            ArticleList.Add(item);
+        }
+        CvNews.ItemsSource = ArticleList;
+    }
+
+    private void CvNews_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var selectedItem = e.CurrentSelection.FirstOrDefault() as Article;
+        if (selectedItem != null)
+        {
+            Navigation.PushAsync(new NewsDetailPage(selectedItem));
+            ((CollectionView)sender).SelectedItem = null;
+        }
+    }
 }
